3845e3d3c77b0900f923d87296c40f6b0f13cb68
angelsilubun@gmail.com <angelsilubun@gmail.com>
2022-12-07 10:00:32 +0700

    Perubahan dashbpard admin

--- resources/views/admin/admin/dashboard_admin.blade.php
+++ resources/views/admin/admin/dashboard_admin.blade.php
@@ -20,7 +20,15 @@
                     <div class="card-body">
                         <div class="numbers"> {{ $counting_user_pengguna }}</div>
                         <div class="cardName">Pengguna</div>
-                        <a href="" class="text-end mt-2">Lihat semua</a>
+                    </div>
+                    <div class="iconBx card-img-overlay">
+                        <i class="bi bi-person-fill"></i>
+                    </div>
+                </div>
+                <div class="card border-0">
+                    <div class="card-body">
+                        <div class="numbers">12</div>
+                        <div class="cardName">Vendor</div>
                     </div>
                     <div class="iconBx card-img-overlay">
                         <i class="bi bi-person-fill"></i>
@@ -30,7 +38,6 @@
                     <div class="card-body">
                         <div class="numbers">40</div>
                         <div class="cardName">Total Pesanan</div>
-                        <a href="" class="text-end mt-2">Lihat semua</a>
                     </div>
                     <div class="iconBx card-img-overlay">
                         <i class="bi bi-cart-fill"></i>
@@ -40,7 +47,6 @@
                     <div class="card-body">
                         <div class="numbers">40</div>
                         <div class="cardName">Penarikan selesai</div>
-                        <a href="" class="text-end mt-2">Lihat semua</a>
                     </div>
                     <div class="iconBx card-img-overlay">
                         <i class="bi bi-credit-card-fill"></i>
@@ -53,7 +59,7 @@
                     <div class="row col-md-12 " style="font-size: 15px">
                         <div class="col-md-6  px-2 py-2">
                             <div class="card px-2 py-2">
-                                <h2 style="color:#00B56A">Verifikasi Pengguna</h2>
+                                <h5 class="fw-bold">Verifikasi Pengguna</h5>
                                 <table style="width: auto">
                                     <thead class="text-start">
                                         <tr>
@@ -80,7 +86,7 @@
                         </div>
                         <div class="col-md-6 px-2 py-2">
                             <div class="card px-2 py-2">
-                                <h2 style="color:#00B56A">Verifikasi Layanan</h2>
+                                <h5 class="fw-bold">Verifikasi Layanan</h5>
                                 <table style="width: auto">
                                     <thead class="text-start">
                                         <tr>
